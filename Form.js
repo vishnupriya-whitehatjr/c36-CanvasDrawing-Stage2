6756384ca2346d7e07e1c92ad29810134abe26ab
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-04-12 04:45:24 +0530

    commit

--- Form.js
+++ Form.js
@@ -3,6 +3,7 @@ class Form{
         this.button= createButton('Save');
         this.button.style('width','100px');
         this.button.style('height','50px');
+        this.button.style('font-size','20px');
         this.button.style('background-color','lightblue');
         
     }
